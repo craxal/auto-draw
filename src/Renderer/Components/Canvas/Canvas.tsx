8e4c7ab8467a78d5e9a38401a43266aa30c3d16d
Craig Alvord <laxarc@gmail.com>
2023-09-28 22:14:28 -0700

    Make canvas dimensions dynamic

--- src/Renderer/Components/Canvas/Canvas.tsx
+++ src/Renderer/Components/Canvas/Canvas.tsx
@@ -6,6 +6,8 @@ export function Canvas(props: {
     render: (context: DrawContext) => void;
 }): JSX.Element {
     const canvasElementRef = React.useRef<HTMLCanvasElement>(null);
+    const [width, setWidth] = React.useState(800);
+    const [height, setHeight] = React.useState(500);
 
     React.useLayoutEffect(() => {
         if (!!canvasElementRef.current) {
@@ -22,7 +24,21 @@ export function Canvas(props: {
         }
     });
 
+    React.useEffect(() => {
+        const observer = new ResizeObserver((entries) => {
+            setWidth(entries[0].contentBoxSize[0].inlineSize);
+            setHeight(entries[0].contentBoxSize[0].blockSize);
+        });
+        if (!!canvasElementRef.current) {
+            observer.observe(canvasElementRef.current);
+        }
+
+        return () => {
+            observer.disconnect();
+        }
+    }, [width, height]);
+
     return (
-        <canvas className={'canvas'} width={800} height={500} ref={canvasElementRef} />
+        <canvas className={'canvas'} width={width} height={height} ref={canvasElementRef} />
     );
 }
